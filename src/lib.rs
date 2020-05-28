2c5c326712627376ac6ec6f1755892afe94dcfd3
kanapuli <athavankanapuli@gmail.com>
2020-05-28 06:20:15 +0530

    Implement Iterator for Flatten. Change generic var I to O for sanity

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,25 @@ pub fn flatten<I>(iter: I) -> Flatten<I> {
   Flatten::new(iter)
 }
 
-pub struct Flatten<I>{
-  outer: I,
+pub struct Flatten<O>{
+  outer: O,
 }
 
-impl <I> Flatten<I> {
-  fn new(iter: I) -> Self {
+impl <O> Flatten<O> {
+  fn new(iter: O) -> Self {
     Flatten {
       outer: iter
     }
+  }
+}
+
+impl <O> Iterator for Flatten<O> 
+where 
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+  type Item = <O::Item as IntoIterator>::Item;
+  fn next(&mut self) -> Option<Self::Item> {
+    None
   }
 }
