7bb2383bef7d3462a6e35ec5b04dce63231b9f93
kanapuli <athavankanapuli@gmail.com>
2020-05-28 07:05:23 +0530

    Implement next to have an memory of inner iterator and walkthrough inner iterator if it yields an value

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,76 @@
-pub fn flatten<I>(iter: I) -> Flatten<I> {
-  Flatten::new(iter)
+pub fn flatten<I>(iter: I) -> Flatten<I>
+where
+    I: Iterator,
+    I::Item: IntoIterator,
+{
+    Flatten::new(iter)
 }
 
-pub struct Flatten<O>{
-  outer: O,
-}
-
-impl <O> Flatten<O> {
-  fn new(iter: O) -> Self {
-    Flatten {
-      outer: iter
-    }
-  }
-}
-
-impl <O> Iterator for Flatten<O> 
-where 
+pub struct Flatten<O>
+where
     O: Iterator,
     O::Item: IntoIterator,
 {
-  type Item = <O::Item as IntoIterator>::Item;
-  fn next(&mut self) -> Option<Self::Item> {
-    self.outer.next().and_then(|inner| { inner.into_iter().next()})
-  }
+    outer: O,
+    inner: Option<<O::Item as IntoIterator>::IntoIter>,
+}
+
+impl<O> Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator
+{
+    fn new(iter: O) -> Self {
+        Flatten {
+            outer: iter,
+            inner: None,
+        }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(ref mut inner_iter) = self.inner {
+            if let Some(i) = inner_iter.next() {
+                return Some(i);
+            }
+            self.inner = None;
+        }
+        let inner_item = self.outer.next()?;
+        let mut inner_iter = inner_item.into_iter();
+        inner_iter.next()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  #[test]
-  fn empty() {
-    assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count() , 0)
-  }
-  #[test]
-  fn empty_wide() {
-    assert_eq!(flatten(vec![Vec::<()>::new(), vec![], vec![], vec![]].into_iter()).count() , 0)
-  }
-  #[test]
-  fn one_item() {
-    assert_eq!(flatten(std::iter::once(vec!["a"])).count() , 1)
-  }
-  #[test]
-  fn two_item() {
-    assert_eq!(flatten(std::iter::once(vec!["a","b"])).count() , 2)
-  }
-  #[test]
-  fn two_wide() {
-    assert_eq!(flatten( vec![vec!["a"],vec!["b"]].into_iter()).count(), 2)
-  }
+    use super::*;
+    #[test]
+    fn empty() {
+        assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0)
+    }
+    #[test]
+    fn empty_wide() {
+        assert_eq!(
+            flatten(vec![Vec::<()>::new(), vec![], vec![], vec![]].into_iter()).count(),
+            0
+        )
+    }
+    #[test]
+    fn one_item() {
+        assert_eq!(flatten(std::iter::once(vec!["a"])).count(), 1)
+    }
+    #[test]
+    fn two_item() {
+        assert_eq!(flatten(std::iter::once(vec!["a", "b"])).count(), 2)
+    }
+    #[test]
+    fn two_wide() {
+        assert_eq!(flatten(vec![vec!["a"], vec!["b"]].into_iter()).count(), 2)
+    }
 }
